80d35315d89f54022fcadb1b75ac713ac258249d
Alexey Mironov <amironov73@gmail.com>
2019-06-14 09:10:31 +0800

    Add search expression builder (experimental)

---
+++ Source/Search.php
@@ -0,0 +1,318 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * Search expression builder.
+ */
+final class Search
+{
+    private $_buffer = '';
+
+    /**
+     * All documents in the database.
+     * @return string
+     */
+    public static function all() {
+        $result = new Search();
+        $result->_buffer = "I=$";
+        return $result;
+    } // function all
+
+    /**
+     * Logical AND.
+     * @return $this
+     */
+    public function and_() {
+        $this->_buffer = '(' . $this->_buffer;
+        foreach (func_get_args() as $item) {
+            $this->_buffer = $this->_buffer
+                . ' * '
+                . self::wrapIfNeeded($item);
+        }
+        $this->_buffer .= ')';
+        return $this;
+    } // function and_
+
+    /**
+     * Search for matching records.
+     * @param $prefix
+     * @return Search
+     */
+    public static function equals($prefix) {
+        $result = new Search();
+        if (is_array($prefix)) {
+            $values = $prefix;
+            $prefix = $values[0];
+        } else {
+            $values = func_get_args();
+        }
+        if (count($values) < 2)
+            return $result;
+        array_shift($values);
+        if (count($values) > 1) {
+            $result->_buffer = '(';
+        }
+        $result->_buffer .= self::wrapIfNeeded($prefix . $values[0]);
+        array_shift($values);
+        foreach ($values as $value)
+            $result->_buffer = $result->_buffer
+                . ' + '
+                . self::wrapIfNeeded($prefix . $value);
+        if (count($values))
+            $result->_buffer .= ')';
+        return $result;
+    } // function equals
+
+    /**
+     * Need to wrap the text?
+     * @param $text
+     * @return bool
+     */
+    public static function needWrap($text) {
+        $text = (string)$text;
+        if (empty($text))
+            return true;
+        $c = $text[0];
+        if ($c == '"' or $c == '(')
+            return false;
+        if (strpos($text, ' ') !== false
+            or strpos($text, '+') !== false
+            or strpos($text, '*') !== false
+            or strpos($text, '(') !== false
+            or strpos($text, ')') !== false
+            or strpos($text, '"') !== false)
+            return true;
+        return false;
+    } // function needWrap
+
+    /**
+     * Logical NOT.
+     * @param $text
+     * @return $this
+     */
+    public function not($text) {
+        $this->_buffer = '('
+            . $this->_buffer
+            . ' ^ '
+            . self::wrapIfNeeded($text)
+            . ')';
+        return $this;
+    } // function not
+
+    /**
+     * Logical OR.
+     * @return $this
+     */
+    public function or_() {
+        $this->_buffer = '(' . $this->_buffer;
+        foreach (func_get_args() as $item) {
+            $this->_buffer = $this->_buffer
+                . ' + '
+                . self::wrapIfNeeded($item);
+        }
+        $this->_buffer .= ')';
+        return $this;
+    } // function or_
+
+    /**
+     * Search in the same field.
+     * @return $this
+     */
+    public function sameField() {
+        $this->_buffer = '(' . $this->_buffer;
+        foreach (func_get_args() as $item) {
+            $this->_buffer = $this->_buffer
+                . ' (G) '
+                . self::wrapIfNeeded($item);
+        }
+        $this->_buffer .= ')';
+        return $this;
+    } // function sameField
+
+    /**
+     * Search in the same field repeat.
+     * @return $this
+     */
+    public function sameRepeat() {
+        $this->_buffer = '(' . $this->_buffer;
+        foreach (func_get_args() as $item) {
+            $this->_buffer = $this->_buffer
+                . ' (F) '
+                . self::wrapIfNeeded($item);
+        }
+        $this->_buffer .= ')';
+        return $this;
+    } // function sameField
+
+    /**
+     * Wrap the text if needed.
+     * @param $text
+     * @return string
+     */
+    public static function wrapIfNeeded($text) {
+        $value = (string)$text;
+        if (self::needWrap($value))
+            return '"' . $value . '"';
+        return $value;
+    } // function wrapIfNeeded
+
+    public function __toString() {
+        return $this->_buffer;
+    } // function __toString
+
+} // class Search
+
+function keyword() {
+    $args = func_get_args();
+    array_unshift($args, 'K=');
+    return Search::equals($args);
+} // function keyword
+
+function author() {
+    $args = func_get_args();
+    array_unshift($args, 'A=');
+    return Search::equals($args);
+} // function author
+
+function title() {
+    $args = func_get_args();
+    array_unshift($args, 'T=');
+    return Search::equals($args);
+} // function title
+
+function number() {
+    $args = func_get_args();
+    array_unshift($args, 'IN=');
+    return Search::equals($args);
+} // function number
+
+function publisher() {
+    $args = func_get_args();
+    array_unshift($args, 'O=');
+    return Search::equals($args);
+} // function publisher
+
+function place() {
+    $args = func_get_args();
+    array_unshift($args, 'MI=');
+    return Search::equals($args);
+} // function place
+
+function subject() {
+    $args = func_get_args();
+    array_unshift($args, 'S=');
+    return Search::equals($args);
+} // function subject
+
+function language() {
+    $args = func_get_args();
+    array_unshift($args, 'J=');
+    return Search::equals($args);
+} // function language
+
+function year() {
+    $args = func_get_args();
+    array_unshift($args, 'G=');
+    return Search::equals($args);
+} // function year
+
+function magazine() {
+    $args = func_get_args();
+    array_unshift($args, 'TJ=');
+    return Search::equals($args);
+} // function magazine
+
+function documentKind() {
+    $args = func_get_args();
+    array_unshift($args, 'V=');
+    return Search::equals($args);
+} // function documentKind
+
+function udc() {
+    $args = func_get_args();
+    array_unshift($args, 'U=');
+    return Search::equals($args);
+} // function udc
+
+function bbk() {
+    $args = func_get_args();
+    array_unshift($args, 'bbk=');
+    return Search::equals($args);
+} // function bbk
+
+function rzn() {
+    $args = func_get_args();
+    array_unshift($args, 'RZN=');
+    return Search::equals($args);
+} // function rzn
+
+function mhr() {
+    $args = func_get_args();
+    array_unshift($args, 'MHR=');
+    return Search::equals($args);
+} // function mhr
+
+echo "<pre>\n";
+
+echo Search::wrapIfNeeded('') . "\n";
+echo Search::wrapIfNeeded('Hello') . "\n";
+echo Search::wrapIfNeeded('Hello, world') . "\n";
+
+echo "\n";
+
+echo Search::equals('K=', '1') . "\n";
+echo Search::equals('K=', '1', '2') . "\n";
+echo Search::equals('K=', '1', '2', '3') . "\n";
+
+echo "\n";
+
+echo Search::equals('K=', 'Hello, world') . "\n";
+
+echo "\n";
+
+echo Search::equals('K=', '1')
+    ->and_(Search::equals('T=', 'Byron')) . "\n";
+
+echo "\n";
+
+echo Search::equals('K=', '1')
+    ->and_(Search::equals('T=', 'Byron'),
+        Search::equals('G=', '2019')) . "\n";
+
+echo "\n";
+
+echo Search::equals('K=', '1')
+    ->and_(Search::equals('T=', 'Byron'),
+        Search::equals('G=', '2019'))
+    ->or_('J=KOR') . "\n";
+
+echo "\n";
+
+echo Search::equals('K=', '1')
+    ->and_(Search::equals('T=', 'Byron'),
+        Search::equals('G=', '2019'))
+    ->not('J=KOR') . "\n";
+
+echo "\n";
+
+echo Search::equals('K=', '1')
+    ->sameField(Search::equals('T=', 'Byron'),
+        Search::equals('G=', '2019')) . "\n";
+
+echo "\n";
+
+echo Search::equals('K=', '1')
+    ->sameRepeat(Search::equals('T=', 'Byron'),
+        Search::equals('G=', '2019')) . "\n";
+
+echo "\n";
+
+echo keyword('1', '2', '3') . "\n";
+echo keyword('1', '2')->and_(title('Byron')) . "\n";
+echo keyword('1', '2')->not(title('Byron')) . "\n";
+echo keyword(1)->or_(title(2))->or_(publisher(3))->and_(number(4)) . "\n";
+
+echo "\n";
+
+echo "</pre>\n";
